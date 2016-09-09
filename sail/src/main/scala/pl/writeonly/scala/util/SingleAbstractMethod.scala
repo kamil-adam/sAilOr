ca852a64024afa72b33d102561afbf271b9b847e
writeonly <enkidu616@gmail.com>
2016-09-09 07:54:19 +0200

    Update SingleAbstractMethod.scala

--- sail/src/main/scala/pl/writeonly/scala/util/SingleAbstractMethod.scala
+++ sail/src/main/scala/pl/writeonly/scala/util/SingleAbstractMethod.scala
@@ -1,9 +1,11 @@
 package pl.writeonly.scala.util
 
+  //FuctionalInterface
 object SingleAbstractMethod {
   implicit def toRunnable(unit: () => Unit) = new Runnable() { def run() = unit() }
 }
 
+  //FuctionalInterface
 class SingleAbstractMethod {
   implicit def toRunnable(unit: () => Unit) = new Runnable() { def run() = unit() }
 }
